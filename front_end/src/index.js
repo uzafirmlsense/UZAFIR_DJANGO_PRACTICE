4261e1d8c32d6d4d81cfcea723d5b3156252f095
Mirza Ahmer Gull <mirzaahmergull@outlook.com>
2022-08-15 15:34:26 +0500

    Modifies some frontend files

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "App";
+import { MaterialUIControllerProvider } from "context";
+import App from "./App";
 
 // Material Dashboard 2 PRO React Context Provider
-import { MaterialUIControllerProvider } from "context";
-
 ReactDOM.render(
   <BrowserRouter>
     <MaterialUIControllerProvider>
