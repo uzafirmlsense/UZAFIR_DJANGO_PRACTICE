49c1b97fdf8922cbae9ba44ec53f5e3bac6a42a0
uzafirmlsense <uzafir.ahmad@mlsense.ai>
2023-04-11 14:39:52 +0500

    fixed secret

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#)g#r#@6rckvrsd6budy2_id7&2yr)y@_nn^!^)^pb86jk)5hm'
+# SECRET_KEY = 'django-insecure-#)g#r#@6rckvrsd6budy2_id7&2yr)y@_nn^!^)^pb86jk)5hm'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', None),
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
